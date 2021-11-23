92e3486032a43da459233316bf675abf42e62e5c
Krzysztof.Lerski <krzysztof.lerski@asseco.pl>
2021-11-23 11:16:49 +0100

    tests

--- qajunit/src/test/java/pl/jsystems/qa/qajunit/JunitParamTest.java
+++ qajunit/src/test/java/pl/jsystems/qa/qajunit/JunitParamTest.java
@@ -5,6 +5,8 @@ import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Tag;
 import org.junit.jupiter.api.Tags;
 import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvFileSource;
+import org.junit.jupiter.params.provider.CsvSource;
 import org.junit.jupiter.params.provider.ValueSource;
 import static com.google.common.truth.Truth.assertThat;
 
@@ -22,6 +24,23 @@ public class JunitParamTest {
     @ValueSource(strings = {"Hello","Hello junit","Helo students"})
     public void stringParamTest(String param) {
         assertThat(param).contains("Hello");
+    }
+
+    @DisplayName("Parameter test with multi param")
+    @ParameterizedTest(name = "Parameter test: {0}, {1}")
+    @CsvSource(value = {"Hello; 5","Hello, junit; 15", "Hello students; 25"}, delimiter = ';') //ozna uzyc ' do wydzielania grup ktore tez korzystaja z delimitera
+    public void csvParamTest(String param, int number) {
+        assertThat(param).contains("Hello");
+        Assert.assertEquals(number % 5, 0);
+    }
+
+    //mozna też przygotowav plik csv
+    @DisplayName("Parameter test with multi param, csv file")
+    @ParameterizedTest(name = "Parameter test: {0}, {1}")
+    @CsvFileSource(resources = "/plik.csv", delimiter = ';')
+    public void multiCsvParamTest(String param, int number) {
+        assertThat(param).contains("Hello");
+        Assert.assertEquals(number % 5, 0);
     }
 
 }
