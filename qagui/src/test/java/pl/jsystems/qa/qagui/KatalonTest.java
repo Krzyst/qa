b6ae985e7b990eeb24d0ea048005307f73946021
Krzysztof.Lerski <krzysztof.lerski@asseco.pl>
2021-11-24 10:25:36 +0100

    tests

--- qagui/src/test/java/pl/jsystems/qa/qagui/KatalonTest.java
+++ qagui/src/test/java/pl/jsystems/qa/qagui/KatalonTest.java
@@ -45,11 +45,11 @@ public class KatalonTest {
         driver.findElement(By.linkText("Log In")).click();
         driver.findElement(By.id("usernameOrEmail")).click();
         driver.findElement(By.id("usernameOrEmail")).clear();
-        driver.findElement(By.id("usernameOrEmail")).sendKeys("automation112021");
+        driver.findElement(By.id("usernameOrEmail")).sendKeys("krzyst");
         driver.findElement(By.xpath("//button[@type='submit']")).click();
         driver.findElement(By.id("password")).click();
         driver.findElement(By.id("password")).clear();
-        driver.findElement(By.id("password")).sendKeys("Test112021");
+        driver.findElement(By.id("password")).sendKeys("TestSelenium");
         driver.findElement(By.xpath("//button[@type='submit']")).click();
         driver.findElement(By.xpath("(.//*[normalize-space(text()) and normalize-space(.)='Nowy wpis'])[1]/following::*[name()='svg'][1]")).click();
         driver.findElement(By.xpath("//div[@id='secondary']/ul/li/div[2]/button")).click();
