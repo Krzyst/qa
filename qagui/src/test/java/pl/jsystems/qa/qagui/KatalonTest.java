6d95a9357b84fc85c3cacc518b582e1702085d02
Krzysztof.Lerski <krzysztof.lerski@asseco.pl>
2021-11-24 10:18:33 +0100

    tests

--- qagui/src/test/java/pl/jsystems/qa/qagui/KatalonTest.java
+++ qagui/src/test/java/pl/jsystems/qa/qagui/KatalonTest.java
@@ -1,10 +1,64 @@
 package pl.jsystems.qa.qagui;
 
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+
+import java.net.URISyntaxException;
+import java.nio.file.Paths;
+import java.util.concurrent.TimeUnit;
 
 public class KatalonTest {
 
-    @Test
+    private WebDriver driver;
 
+    @BeforeEach
+    public void setUp() {
+
+        try {
+            System.setProperty("webdriver.gecko.driver", Paths.get(getClass().getClassLoader()
+                    .getResource("drivers/geckodriver.exe").toURI()).toFile().getAbsolutePath());
+
+//            System.setProperty("webdriver.chrome.driver", Paths.get(getClass().getClassLoader()
+//                    .getResource("drivers/chromedriver.exe").toURI()).toFile().getAbsolutePath());
+
+        } catch (URISyntaxException e) {
+            e.printStackTrace();
+        }
+
+        driver = new FirefoxDriver();
+//        driver = new ChromeDriver();
+        driver.manage().timeouts().implicitlyWait(15, TimeUnit.SECONDS);
+        driver.manage().timeouts().pageLoadTimeout(30, TimeUnit.SECONDS);
+        driver.manage().deleteAllCookies();
+        driver.manage().window().maximize();
+
+    }
+
+    @Test
+    public void testUntitledTestCase() throws Exception {
+        driver.get("https://wordpress.com/");
+        driver.findElement(By.linkText("Log In")).click();
+        driver.findElement(By.id("usernameOrEmail")).click();
+        driver.findElement(By.id("usernameOrEmail")).clear();
+        driver.findElement(By.id("usernameOrEmail")).sendKeys("automation112021");
+        driver.findElement(By.xpath("//button[@type='submit']")).click();
+        driver.findElement(By.id("password")).click();
+        driver.findElement(By.id("password")).clear();
+        driver.findElement(By.id("password")).sendKeys("Test112021");
+        driver.findElement(By.xpath("//button[@type='submit']")).click();
+        driver.findElement(By.xpath("(.//*[normalize-space(text()) and normalize-space(.)='Nowy wpis'])[1]/following::*[name()='svg'][1]")).click();
+        driver.findElement(By.xpath("//div[@id='secondary']/ul/li/div[2]/button")).click();
+        //ERROR: Caught exception [unknown command []]
+    }
+
+        @AfterEach
+    public void tearDown() {
+        driver.quit();
+        }
 
 }
